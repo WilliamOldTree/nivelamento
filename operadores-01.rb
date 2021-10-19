8c32e5786b06f675d7d27212635e859f2aeace03
WilliamOldTree <dwbarbosa@rd.com.br>
2021-10-19 15:32:35 -0300

    third commit

--- operadores-01.rb
+++ operadores-01.rb
@@ -9,7 +9,10 @@
 
 #- Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos?
 
-puts 24 * 365
-puts 60*24*365*10
-puts 3600*24*365*47
-puts 1232000000/(3600*24*365)
+puts "Um ano tem", 24 * 365
+puts
+puts"Uma decada tem",  60*24*365*10
+puts
+puts"minha idade em segundos é", 47*365*24*60*60
+puts
+puts "Minha idade é ", 1232000000/(3600*24*365)
--- triangulo.rb
+++
@@ -1,4 +0,0 @@
-#entre com tres lado do triangulo e calcule sua area
-
-puts "Entre com o lado A do triangulo"
-    
