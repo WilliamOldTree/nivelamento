e4806c7738bf180546193889145b417e3f29d8be
WilliamOldTree <dwbarbosa@rd.com.br>
2021-10-18 21:10:07 -0300

    first commit

---
+++ operadores-01.rb
@@ -0,0 +1,15 @@
+#Escreva um programa que calcula e exiba na tela:
+
+#- quantas horas há em um ano?
+#- quantos minutos há em uma década?
+#- qual é a sua idade em segundos?
+
+
+#Desafio:
+
+#- Se minha idade é de 1232 milhões de segundos, qual é minha idade em anos?
+
+puts 24 * 365
+puts 60*24*365*10
+puts 3600*24*365*47
+puts 1232000/(3600*24*365)
